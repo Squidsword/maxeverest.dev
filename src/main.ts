93aad1ee3c693edc550d9d207913bbea7bcf427a
Squidsword <messagemarvin@gmail.com>
2023-05-12 15:59:37 -0400

    Make the torus glow if a link is being hovered on

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio)
 camera.position.setZ(30)
 
+
 const geometry = new THREE.TorusGeometry(20, 4.5, 15, 100)
 const material = new THREE.MeshBasicMaterial({ color: 0x999999, wireframe: true })
 const torus = new THREE.Mesh(geometry, material);
@@ -23,16 +24,16 @@ torus.position.z = 3
 scene.add(torus)
 scene.background = new THREE.Color("rgb(32,32,32)")
 
-var frames = 0
-
 function animate() {
   requestAnimationFrame(animate)
-  torus.rotation.x = 2.5 - Math.cos(frames / 600) / 4
+  torus.rotation.x = 2.5 - Math.cos(performance.now() / 4000) / 4
   torus.rotation.z += 0.0004
+  updateTorusColor()
   renderer.render(scene, camera)
-  frames++
 }
 
+
+window.addEventListener('resize', resizeRenderer);
 function resizeRenderer() {
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -41,7 +42,36 @@ function resizeRenderer() {
   camera.updateProjectionMatrix();
 }
 
-window.addEventListener('resize', resizeRenderer);
+
+enum State {
+  TowardsLight,
+  TowardsDark
+}
+
+var s = State.TowardsDark;
+var interpolation = 0;
+
+var style = getComputedStyle(document.body)
+var light = style.getPropertyValue('--light') 
+var dark = style.getPropertyValue('--dark')
+
+var lastFrameTime = performance.now()
+var transitionTime = +style.getPropertyValue('--transition-time').replace('s', '') * 1000
+let transitionSpeed = 1 / transitionTime
+
+function updateTorusColor() {
+  let currFrameTime = performance.now()
+  let timeSinceLastFrame = currFrameTime - lastFrameTime
+  interpolation = Math.min(1, Math.max(0, interpolation + timeSinceLastFrame * (s == State.TowardsLight ? transitionSpeed : -transitionSpeed)))
+  torus.material.color = new THREE.Color(dark).lerp(new THREE.Color(light), interpolation)
+  lastFrameTime = currFrameTime
+}
+
+const listElements = document.querySelectorAll('a');
+listElements.forEach(element => {
+  element.addEventListener('mouseenter', () => s = State.TowardsLight);
+  element.addEventListener('mouseleave', () => s = State.TowardsDark);
+});
 
 resizeRenderer();
 animate()
